cde5528c420ecbf621d6dd29073dc6aacda47ddf
Samuel Morais <morais123samuel@gmail.com>
2025-10-20 22:23:52 -0300

    feat: Adiciona Rota #32

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts.views import UserViewSet
+from products.views import ProductsViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'products', ProductsViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
