62f1acfc3da7b8bc673f2eec67f8829186a16544
Samuel Morais <morais123samuel@gmail.com>
2025-10-20 21:53:38 -0300

    feat: Cria Modelo Para Produtos #32

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    codigo = models.CharField(max_length=20, unique= True)
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.PositiveIntegerField()
+    categoria = models.CharField(max_length = 50)
+    data_validade = models.DateField(null=True, blank= True)
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add= True)
+    atualizado_em = models.DateTimeField(auto_now= True)
+
+    class Meta:
+        ordering = ['-criado_em']
+    def __str__(self):
+        return f"{self.codigo} - {self.nome}"
