d1408184fa1f1c5b2d24cb011cf3f6d6d5edf2cd
Samuel Morais <morais123samuel@gmail.com>
2025-10-21 21:35:44 -0300

    bug: Corrige Divergência Entre Nomes #32

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.produtos_list, name='products_list'),
-    path('create/', views.produtos_create, name='products_create'),
-    path('edit/<int:id>/', views.produtos_edit, name='products_edit'),
+    path('', views.products_list, name='products_list'),
+    path('create/', views.products_create, name='products_create'),
+    path('edit/<int:id>/', views.products_edit, name='products_edit'),
 ]
